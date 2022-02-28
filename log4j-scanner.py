c27b579d73e6149bc001c2d03dfa0bd46552d994
HectorFilipy <hector_f2@hotmail.com>
2022-02-28 14:35:17 -0300

    Primeira versão

---
+++ log4j-scanner.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python3
+#
+#  Arquivo de scan para encontrar Log4j arquivos que estão vulneraveis para Log4Shell (CVE-2021-44228)
+#  Ele faz a varredura recursivamente no disco e nos arquivos Java Archive (JARs).
+#
+#  Exemplo de uso para scanear um path (por padrão /):
+#      $ python3 log4j-finder.py /caminho/do/scan
+#
+#  Ou diretamente um arquivo JAR:
+#      $ python3 log4j-finder.py /caminho/do/jarfile.jar
+#
+#  Ou Mutiplos diretórios:
+#      $ python3 log4j-finder.py /caminho/do/dir1 /caminho/do/dir2
+#
+#
+import os
+import io
+import sys
+import time
+import zipfile
+import logging
+import argparse
+import hashlib
+import platform
+import datetime
+import functools
+import itertools
+import collections
+
+from pathlib import Path
+
+__version__ = "1.0.2"
+FIGLET = f"""\
+ __               _____  __         ___ __           __
+|  |.-----.-----.|  |  ||__|______.'  _|__|.-----.--|  |.-----.----.
+|  ||  _  |  _  ||__    |  |______|   _|  ||     |  _  ||  -__|   _|
+|__||_____|___  |   |__||  |      |__| |__||__|__|_____||_____|__|
+          |_____|      |___| v{__version__}
+"""
+
+# Opcionalmente, importe o colorama para habilitar a saída colorida para Windows.
+try:
+    import colorama
+
+    colorama.init()
+    NO_COLOR = False
+except ImportError:
+    NO_COLOR = True if sys.platform == "win32" else False
+
+log = logging.getLogger(__name__)
+
+# Extensões de arquivos Java
+JAR_EXTENSIONS = (".jar", ".war", ".ear")
+
+# Nomes de arquivos a serem encontrados e hash MD5 (também recursivamente em JAR_EXTENSIONS)
+# Atualmente, procuramos apenas JndiManager.class
+FILENAMES = [
+    p.lower()
+    for p in [
+        "JndiManager.class",
+    ]
+]
+
+# Mal conhecido
+MD5_BAD = {
+    # JndiManager.class (source: https://github.com/nccgroup/Cyber-Defence/blob/master/Intelligence/CVE-2021-44228/modified-classes/md5sum.txt)
+    "04fdd701809d17465c17c7e603b1b202": "log4j 2.9.0 - 2.11.2",
+    "21f055b62c15453f0d7970a9d994cab7": "log4j 2.13.0 - 2.13.3",
+    "3bd9f41b89ce4fe8ccbf73e43195a5ce": "log4j 2.6 - 2.6.2",
+    "415c13e7c8505fb056d540eac29b72fa": "log4j 2.7 - 2.8.1",
+    "5824711d6c68162eb535cc4dbf7485d3": "log4j 2.12.0 - 2.12.1",
+    "6b15f42c333ac39abacfeeeb18852a44": "log4j 2.1 - 2.3",
+    "8b2260b1cce64144f6310876f94b1638": "log4j 2.4 - 2.5",
+    "a193703904a3f18fb3c90a877eb5c8a7": "log4j 2.8.2",
+    "f1d630c48928096a484e4b95ccb162a0": "log4j 2.14.0 - 2.14.1",
+    # 2.15.0 vulneraveis a ataque de negação de serviço (source: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-45046)
+    "5d253e53fa993e122ff012221aa49ec3": "log4j 2.15.0",
+}
+
+# Bem conhecido
+MD5_GOOD = {
+    # JndiManager.class (source: https://repo.maven.apache.org/maven2/org/apache/logging/log4j/log4j-core/2.16.0/log4j-core-2.16.0.jar)
+    "ba1cf8f81e7b31c709768561ba8ab558": "log4j 2.16.0",
+}
+
+HOSTNAME = platform.node()
+
+
+def md5_digest(fobj):
+    """Calcule o MD5 de um objeto de arquivo."""
+    d = hashlib.md5()
+    for buf in iter(functools.partial(fobj.read, io.DEFAULT_BUFFER_SIZE), b""):
+        d.update(buf)
+    return d.hexdigest()
+
+
+def iter_scandir(path, stats=None):
+    """
+    Rende todos os arquivos correspondentes a JAR_EXTENSIONS ou FILENAMES recursivamente no caminho
+    """
+    p = Path(path)
+    if p.is_file():
+        if stats:
+            stats["files"] += 1
+        yield p
+    try:
+        for entry in scantree(path, stats=stats):
+            if entry.is_symlink():
+                continue
+            elif entry.is_file():
+                name = entry.name.lower()
+                if name.endswith(JAR_EXTENSIONS):
+                    yield Path(entry.path)
+                elif name in FILENAMES:
+                    yield Path(entry.path)
+    except IOError as e:
+        log.debug(e)
+
+
+def scantree(path, stats=None):
+    """Rende recursivamente objetos DirEntry para determinado diretório."""
+    try:
+        with os.scandir(path) as it:
+            for entry in it:
+                if entry.is_dir(follow_symlinks=False):
+                    if stats:
+                        stats["directories"] += 1
+                    yield from scantree(entry.path, stats=stats)
+                else:
+                    if stats:
+                        stats["files"] += 1
+                    yield entry
+    except IOError as e:
+        log.debug(e)
+
+
+def iter_jarfile(fobj, parents=None, stats=None):
+    """
+    Rendimentos (zfile, zinfo, zpath, pais) para cada arquivo em zipfile que corresponda a `FILENAMES` ou` JAR_EXTENSIONS` (recursivamente)
+    """
+    parents = parents or []
+    try:
+        with zipfile.ZipFile(fobj) as zfile:
+            for zinfo in zfile.infolist():
+                # log.debug(zinfo.filename)
+                zpath = Path(zinfo.filename)
+                if zpath.name.lower() in FILENAMES:
+                    yield (zinfo, zfile, zpath, parents)
+                elif zpath.name.lower().endswith(JAR_EXTENSIONS):
+                    zfobj = zfile.open(zinfo.filename)
+                    try:
+                        # Teste se podemos abrir o zfobj sem erros, fallback para BytesIO caso contrário
+                        zipfile.ZipFile(zfobj)
+                    except zipfile.BadZipFile as e:
+                        log.debug(f"Got {zinfo}: {e}, falling back to BytesIO")
+                        zfobj = io.BytesIO(zfile.open(zinfo.filename).read())
+                    yield from iter_jarfile(zfobj, parents=parents + [zpath])
+    except IOError as e:
+        log.debug(f"{fobj}: {e}")
+    except zipfile.BadZipFile as e:
+        log.debug(f"{fobj}: {e}")
+
+
+def red(s):
+    if NO_COLOR:
+        return s
+    return f"\033[31m{s}\033[0m"
+
+
+def green(s):
+    if NO_COLOR:
+        return s
+    return f"\033[32m{s}\033[0m"
+
+
+def yellow(s):
+    if NO_COLOR:
+        return s
+    return f"\033[33m{s}\033[0m"
+
+
+def cyan(s):
+    if NO_COLOR:
+        return s
+    return f"\033[36m{s}\033[0m"
+
+
+def magenta(s):
+    if NO_COLOR:
+        return s
+    return f"\033[35m{s}\033[0m"
+
+
+def bold(s):
+    if NO_COLOR:
+        return s
+    return f"\033[1m{s}\033[0m"
+
+
+def check_vulnerable(fobj, path_chain, stats, has_jndilookup=True):
+    """
+    Teste se fobj corresponde a algum dos hashes MD5 conhecidos ou ruins.
+    Também imprime mensagem se fobj for vulnerável ou bom ou desconhecido.
+
+    se `has_jndilookup` for False, significa que` lookup / JndiLookup.class` não foi encontrado e pode
+    indicar que foi corrigido de acordo com https://logging.apache.org/log4j/2.x/security.html usando:
+        zip -q -d log4j-core - *. jar org / apache / logging / log4j / core / lookup / JndiLookup.class
+    """
+    md5sum = md5_digest(fobj)
+    first_path = bold(path_chain.pop(0))
+    path_chain = " -> ".join(str(p) for p in [first_path] + path_chain)
+    comment = collections.ChainMap(MD5_BAD, MD5_GOOD).get(md5sum, "Unknown MD5")
+    color_map = {"vulnerable": red, "good": green, "patched": cyan, "unknown": yellow}
+    if md5sum in MD5_BAD:
+        status = "vulnerable" if has_jndilookup else "patched"
+    elif md5sum in MD5_GOOD:
+        status = "good"
+    else:
+        status = "unknown"
+    stats[status] += 1
+    color = color_map.get(status, red)
+    now = datetime.datetime.utcnow().replace(microsecond=0)
+    hostname = magenta(HOSTNAME)
+    status = bold(color(status.upper()))
+    md5sum = color(md5sum)
+    comment = bold(color(comment))
+    print(f"[{now}] {hostname} {status}: {path_chain} [{md5sum}: {comment}]")
+
+
+def print_summary(stats):
+    print("\nResumo:")
+    print(f" Processado {stats['files']} arquivos e {stats['directories']} diretorios")
+    print(f" Scaneado {stats['scanned']} arquivos")
+    if stats["vulnerable"]:
+        print("  Encontrado {} arquivos vulneraveis".format(stats["vulnerable"]))
+    if stats["good"]:
+        print("  Encontrado {} arquivos bons".format(stats["good"]))
+    if stats["patched"]:
+        print("  Encontrado {} arquivos corrigidos".format(stats["patched"]))
+    if stats["unknown"]:
+        print("  Encontrado {} arquivos desconhecidos".format(stats["unknown"]))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=f"%(prog)s v{__version__} - Encontrar vulneravel log4j2 no sistema de arquivos (Log4Shell CVE-2021-4428, CVE-2021-45046)",
+        epilog="Os arquivos são verificados recursivamente, tanto em disco quanto em Java Archive Files",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "path",
+        metavar="PATH",
+        nargs="*",
+        default=["/"],
+        help="Diretório ou arquivo(s) para verificar (recursivamente)",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Saida detalhada (-v is info, -vv is debug)",
+    )
+    parser.add_argument(
+        "-n", "--no-color", action="store_true", help="desativar a saída de cores"
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="fique mais quieto, desativa banner e resumo.",
+    )
+    parser.add_argument("-b", "--no-banner", action="store_true", help="desativar banner")
+    parser.add_argument(
+        "-V", "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+    args = parser.parse_args()
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+    )
+    if args.verbose == 1:
+        log.setLevel(logging.INFO)
+        log.info("info logging ativado")
+    elif args.verbose >= 2:
+        log.setLevel(logging.DEBUG)
+        log.debug("debug logging ativado")
+
+    if args.no_color:
+        global NO_COLOR
+        NO_COLOR = True
+
+    stats = collections.Counter()
+    start_time = time.monotonic()
+    hostname = magenta(HOSTNAME)
+
+    if not args.no_banner and not args.quiet:
+        print(FIGLET)
+    for directory in args.path:
+        now = datetime.datetime.utcnow().replace(microsecond=0)
+        if not args.quiet:
+            print(f"[{now}] {hostname} Escaneando: {directory}")
+        for p in iter_scandir(directory, stats=stats):
+            if p.name.lower() in FILENAMES:
+                stats["scanned"] += 1
+                log.info(f"Arquivo encontrado: {p}")
+                with p.open("rb") as fobj:
+                    # Se encontrarmos JndiManager, também verificamos se JndiLookup.class existe
+                    has_lookup = True
+                    if p.name.lower().endswith("JndiManager.class".lower()):
+                        lookup_path = p.parent.parent / "lookup/JndiLookup.class"
+                        has_lookup = lookup_path.exists()
+                    check_vulnerable(fobj, [p], stats, has_lookup)
+            if p.suffix.lower() in JAR_EXTENSIONS:
+                try:
+                    log.info(f"Arquivo jar encontrado: {p}")
+                    stats["scanned"] += 1
+                    for (zinfo, zfile, zpath, parents) in iter_jarfile(
+                        p.resolve().open("rb"), parents=[p.resolve()]
+                    ):
+                        log.info(f"zfile encontrado: {zinfo} ({parents}")
+                        with zfile.open(zinfo.filename) as zf:
+                            # Se encontrarmos JndiManager.class, também verificamos se JndiLookup.class existe
+                            has_lookup = True
+                            if zpath.name.lower().endswith("JndiManager.class".lower()):
+                                lookup_path = str(
+                                    zpath.parent.parent / "lookup/JndiLookup.class"
+                                )
+                                try:
+                                    has_lookup = zfile.open(lookup_path)
+                                except KeyError:
+                                    has_lookup = False
+                            check_vulnerable(zf, parents + [zpath], stats, has_lookup)
+                except IOError as e:
+                    log.debug(f"{p}: {e}")
+
+    elapsed = time.monotonic() - start_time
+    now = datetime.datetime.utcnow().replace(microsecond=0)
+    if not args.quiet:
+        print(f"[{now}] {hostname} Scan finalizado, tempo decorrido: {elapsed:.2f} segundos")
+        print_summary(stats)
+        print(f"\nTempo decorrido: {elapsed:.2f} segundos")
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        print("\nAbortado!")
